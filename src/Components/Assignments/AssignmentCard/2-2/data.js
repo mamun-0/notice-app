e2af7ccaa441a732ac400d7251826f0d7b4f5658
mamun-0 <md.abdullahalmamun200@gmail.com>
2021-09-21 11:04:35 +0600

    Math assignment 2-2

--- src/Components/Assignments/AssignmentCard/2-2/data.js
+++ src/Components/Assignments/AssignmentCard/2-2/data.js
@@ -15,5 +15,13 @@ const ASSIGNMENT_22 = [
     downloadLink:
       "https://drive.google.com/file/d/1-ZlAVxhR_dNxpGAOYCV_f53pCCejmb-g/view?usp=sharing",
   },
+  {
+    id: 3,
+    subjectCode: "MAT 255",
+    teacherName: "Nahida Akter",
+    subjectName: "Complex Variables and Laplace",
+    downloadLink:
+      "https://mega.nz/file/7KADBIgC#mPSscqgzohoyhzi8zTi8xXlSUlCCZSlLcrZwJ8ySifI",
+  },
 ];
 export default ASSIGNMENT_22;
