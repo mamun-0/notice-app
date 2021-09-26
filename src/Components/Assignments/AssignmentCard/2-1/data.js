601f9dc6c49dffe52bf88f62119dc1d82fc2e459
mamun-0 <md.abdullahalmamun200@gmail.com>
2021-09-26 22:05:03 +0600

    DLD solution

--- src/Components/Assignments/AssignmentCard/2-1/data.js
+++ src/Components/Assignments/AssignmentCard/2-1/data.js
@@ -15,5 +15,13 @@ const ASSIGNMENT_21 = [
     downloadLink:
       "https://drive.google.com/file/d/1U7J4epK5YwfL9Ux0nEQuKzdPVDRhdpOf/view?usp=sharing",
   },
+  {
+    id: 3,
+    subjectCode: "CSE 203",
+    teacherName: "Abu Bakar Muhammad Abdullah",
+    subjectName: "Digital Logic Design[Solution]",
+    downloadLink:
+      "https://drive.google.com/file/d/13uPAeU2YxvV2gRPzh5m_DjWPZa4r48p8/view?usp=sharing",
+  },
 ];
 export default ASSIGNMENT_21;
