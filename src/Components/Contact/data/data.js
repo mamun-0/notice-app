1bbabf4ccdfd68ac8429288019b4400d6416e6c4
mamun-0 <md.abdullahalmamun200@gmail.com>
2021-12-17 11:13:46 +0600

    update student data

--- src/Components/Contact/data/data.js
+++ src/Components/Contact/data/data.js
@@ -1,10 +1,24 @@
 const information = [
   {
+    id: "18CSE001",
+    name: "Suchana",
+    mobile: "0172875****",
+    email: "sumaiyasuchana97@gmail.com",
+    district: "Jashore",
+  },
+  {
     id: "18CSE002",
     name: "Bishworup",
     mobile: "01771106071",
     email: "bishworupmollik@gmail.com",
     district: "Sylhet",
+  },
+  {
+    id: "18CSE007",
+    name: "Delower",
+    mobile: "01650004337",
+    email: "bsmrstudelower18cse007@gmail.com",
+    district: "Rajshahi",
   },
   {
     id: "18CSE008",
@@ -21,6 +35,27 @@ const information = [
     district: "Meherpur",
   },
   {
+    id: "18CSE012",
+    name: "Fuadul",
+    mobile: "01754726519",
+    email: "fuadul202@gmail.com",
+    district: "Rajbari",
+  },
+  {
+    id: "18CSE018",
+    name: "Juwel",
+    mobile: "01630786586",
+    email: "12juwel34@gmail.com",
+    district: "Magura",
+  },
+  {
+    id: "18CSE019",
+    name: "Imran",
+    mobile: "01819984688",
+    email: "imranhosen.bsmrstu@gmail.com",
+    district: "Cumilla",
+  },
+  {
     id: "18CSE026",
     name: "Abdul Aziz",
     mobile: "01797399432",
@@ -33,6 +68,27 @@ const information = [
     mobile: "01760172264",
     email: "alamin18cse028@gmail.com",
     district: "Jhenaidah",
+  },
+  {
+    id: "18CSE029",
+    name: "Sumon",
+    mobile: "01705937516",
+    email: "sumoncse516@gmail.com",
+    district: "Dinajpur",
+  },
+  {
+    id: "18CSE031",
+    name: "Mizan",
+    mobile: "01751117917",
+    email: "mizanurmizan395@gmail.com",
+    district: "Nilphamari",
+  },
+  {
+    id: "18CSE033",
+    name: "Noor",
+    mobile: "01972897089",
+    email: "shantonoor900@gmail.com",
+    district: "Khulna",
   },
   {
     id: "18CSE034",
@@ -56,6 +112,20 @@ const information = [
     district: "Gopalganj",
   },
   {
+    id: "18CSE037",
+    name: "Rony",
+    mobile: "01773040081",
+    email: "jahidrony162@gmail.com",
+    district: "Gaibandha",
+  },
+  {
+    id: "18CSE039",
+    name: "Farhana",
+    mobile: "0195212****",
+    email: "farhanabsmrstu39@gmail.com",
+    district: "Jashore",
+  },
+  {
     id: "18CSE042",
     name: "Hasan",
     mobile: "01718503969",
@@ -70,6 +140,13 @@ const information = [
     district: "Bagerhat",
   },
   {
+    id: "18CSE046",
+    name: "MAJABUL",
+    mobile: "01762830527",
+    email: "majabulislam527@gmail.com",
+    district: "Rangpur",
+  },
+  {
     id: "18CSE048",
     name: "Habib",
     mobile: "01551060974",
@@ -77,11 +154,32 @@ const information = [
     district: "Narayanganj",
   },
   {
+    id: "18CSE053",
+    name: "Nazir",
+    mobile: "01773306350",
+    email: "nazir.bsmrstu@gmail.com",
+    district: "Rangpur",
+  },
+  {
     id: "18CSE055",
     name: "Limon(Deen)",
     mobile: "01307065557",
     email: "lmd99815@gmail.com",
     district: "Faridpur",
+  },
+  {
+    id: "18CSE057",
+    name: "Nayan",
+    mobile: "01516194476",
+    email: "mizanurrahman.100832@gmail.com",
+    district: "Rajshahi",
+  },
+  {
+    id: "18CSE058",
+    name: "Rabby",
+    mobile: "01758733801",
+    email: "rrabbybhuiyan48@gmail.com",
+    district: "Tangail",
   },
   {
     id: "18CSE061",
@@ -99,7 +197,7 @@ const information = [
   },
   {
     id: "18CSE065",
-    name: "Roni",
+    name: "Rony",
     mobile: "01753915334",
     email: "ronyislam6340@gmail.com",
     district: "Chapai Nawabganj",
@@ -117,6 +215,13 @@ const information = [
     mobile: "01780927573",
     email: "shahriarkawsik@gmail.com",
     district: "Mymensingh",
+  },
+  {
+    id: "18CSE074",
+    name: "Akif",
+    mobile: "01764070685",
+    email: "ibnulakifcse@gmail.com",
+    district: "Narayanganj",
   },
 ];
 export default information;
