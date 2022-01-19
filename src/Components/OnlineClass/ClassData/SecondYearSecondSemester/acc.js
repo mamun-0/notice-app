762497f9e8f7398099f2879f62ba2e366f1d982a
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-01-19 20:24:49 +0600

    Acc video and file update

--- src/Components/OnlineClass/ClassData/SecondYearSecondSemester/acc.js
+++ src/Components/OnlineClass/ClassData/SecondYearSecondSemester/acc.js
@@ -1,18 +1,28 @@
-const ACC_DATA =  [
+const ACC_DATA = [
+  // {
+  //   id: 1,
+  //   videoUrl: "https://www.youtube.com/embed/zI_zliTPTnc",
+  //   videoTitle: "ACC 255 Lecture 1",
+  // },
+  // {
+  //   id: 2,
+  //   videoUrl: "https://www.youtube.com/embed/_hj9mq9Z-Cs",
+  //   videoTitle: "ACC 255 Lecture 2",
+  // },
+  // {
+  //   id: 3,
+  //   videoUrl: "https://www.youtube.com/embed/7VgMvJIf3w0",
+  //   videoTitle: "ACC 255 Lecture 3",
+  // },
   {
     id: 1,
-    videoUrl: "https://www.youtube.com/embed/zI_zliTPTnc",
+    videoUrl: "https://www.youtube.com/embed/zf3R0b18Ozw",
     videoTitle: "ACC 255 Lecture 1",
   },
   {
     id: 2,
-    videoUrl: "https://www.youtube.com/embed/_hj9mq9Z-Cs",
+    videoUrl: "https://www.youtube.com/embed/qxMkevpKVQM",
     videoTitle: "ACC 255 Lecture 2",
   },
-  {
-    id: 3,
-    videoUrl: "https://www.youtube.com/embed/7VgMvJIf3w0",
-    videoTitle: "ACC 255 Lecture 3",
-  },
-]
+];
 export default ACC_DATA;
