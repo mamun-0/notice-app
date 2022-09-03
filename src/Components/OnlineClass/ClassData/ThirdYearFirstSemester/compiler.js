3fbc32c67980481bd720407bbd892d7664f8f0e4
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-09-03 08:41:30 +0600

    compiler design 2nd class

--- src/Components/OnlineClass/ClassData/ThirdYearFirstSemester/compiler.js
+++ src/Components/OnlineClass/ClassData/ThirdYearFirstSemester/compiler.js
@@ -4,5 +4,10 @@ const COMPILER_DATA = [
     videoUrl: 'https://www.youtube.com/embed/msZqlMD48yc',
     videoTitle: 'Compiler Design 1',
   },
+  {
+    id: 2,
+    videoUrl: 'https://www.youtube.com/embed/vDzcOEiSHvQ',
+    videoTitle: 'Compiler Design 2',
+  },
 ];
 export default COMPILER_DATA;
