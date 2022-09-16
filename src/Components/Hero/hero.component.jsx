c990a5308b972a633fad4b46b4d675d844ac6812
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-09-16 23:29:28 +0600

    adding headline in the hero section

--- src/Components/Hero/hero.component.jsx
+++ src/Components/Hero/hero.component.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import "./hero.style.css";
+import React from 'react';
+import './hero.style.css';
 const Hero = () => (
   <section className='hero background-img grid'>
     <div className='hero__left'>
-      <h1>We born to live and survive</h1>
-      <p>
-        তোমরা আদর্শবান হও, সৎ পথে থেকো, মনে রেখো, মুখে হাসি, বুকে বল, তেজে ভরা
-        মন, মানুষ হইতে হবে, মানুষ যখন।
-      </p>
+      <h1>Remember that failure is an event, not a person.</h1>
+      <p>Anyone who has never made a mistake has never tried anything new.</p>
     </div>
   </section>
 );
